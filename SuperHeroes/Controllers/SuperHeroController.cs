82dea7594936f5651929d70456e9fef678524667
natek <natekinderman16@gmail.com>
2019-08-29 13:27:03 -0500

    Created asp.net class by following video and insert my first hero to the database

---
+++ SuperHeroes/Controllers/SuperHeroController.cs
@@ -0,0 +1,99 @@
+﻿using SuperHeroes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroes.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        ApplicationDbContext contex;
+        public SuperHeroController()
+        {
+            contex = new ApplicationDbContext();
+        }
+        // GET: SuperHero
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        // GET: SuperHero/Details/5
+        public ActionResult Details(int id)
+        {
+            
+            return View();
+        }
+
+        // GET: SuperHero/Create
+        public ActionResult Create()
+        {
+            SuperHero superhero = new SuperHero();
+            return View(superhero);
+        }
+
+        // POST: SuperHero/Create
+        [HttpPost]
+        public ActionResult Create(SuperHero superhero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                contex.SuperHero.Add(superhero);
+                contex.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHero/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHero/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- SuperHeroes/Models/SuperHero.cs
+++ SuperHeroes/Models/SuperHero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,12 @@ namespace SuperHeroes.Models
 {
     public class SuperHero
     {
+        [Key]
         public int MyProperty { get; set; }
+        public string SuperHeroNAme { get; set; }
+        public string AlterEgoName { get; set; }
+        public string PrimarySuperHeroAbility { get; set; }
+        public string SecondarySuperheroAbility { get; set; }
+        public string CatchPhrase { get; set; }
     }
 }
