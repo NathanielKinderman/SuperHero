133b0b47aaaa953cd2ea3ff4292593687df5c824
natek <natekinderman16@gmail.com>
2019-08-29 11:12:55 -0500

    created reponstory

---
+++ SuperHeroes/Models/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class SuperHero
+    {
+        public int MyProperty { get; set; }
+    }
+}
